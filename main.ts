58c574bfc895096e71dac1aa3ca2eac140cbce0d
Molabe <153329619+Molabe@users.noreply.github.com>
2024-02-28 22:18:21 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,66 @@
+input.onButtonPressed(Button.A, function () {
+    if (character == "A") {
+        basic.clearScreen()
+        game.addScore(1)
+        basic.pause(500)
+        basic.showNumber(game.score())
+        randomChar()
+        basic.pause(3000)
+        basic.showString(character)
+    } else {
+        basic.clearScreen()
+        game.removeLife(1)
+        basic.pause(1000)
+        basic.showNumber(game.life())
+        randomChar()
+        basic.pause(3000)
+        basic.showString(character)
+    }
+})
+
+input.onButtonPressed(Button.AB, function () {
+    game.isRunning()
+    randomChar()
+    basic.pause(3000)
+    basic.showString(character)
+    if (game.life() == 0) {
+        game.gameOver()
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+    if (character == "B") {
+        basic.clearScreen()
+        game.addScore(1)
+        basic.pause(1500)
+        basic.showNumber(game.score())
+        randomChar()
+        basic.pause(3000)
+        basic.showString(character)
+    } else {
+        basic.clearScreen()
+        game.removeLife(1)
+        basic.pause(1000)
+        basic.showNumber(game.life())
+        randomChar()
+        basic.pause(3000)
+        basic.showString(character)
+    }
+})
+
+function randomChar() {
+    basic.pause(500)
+    let characterGenerator = randint(0, 1)
+    if (characterGenerator == 0) {
+        character = "A"
+    } else {
+        character = "B"
+    }
+}
+
+let character = ""
+game.setScore(0)
+game.setLife(2)
+randomChar()
+basic.pause(3000)
+basic.showString(character)
